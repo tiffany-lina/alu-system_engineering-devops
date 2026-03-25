914d913f39301f7b50b97ae7cae27ddca8da32eb
tiffany <t.turate@alustudent.com>
2026-03-25 09:28:29 -0700

    Add ufw firewall script to block all incoming traffic except 22, 80, 443

---
+++ firewall/0-block_all_incoming_traffic_but.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Block all incoming traffic except SSH, HTTP, HTTPS
+
+sudo apt update
+sudo apt install ufw -y
+sudo ufw reset
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
+sudo ufw status verbose
