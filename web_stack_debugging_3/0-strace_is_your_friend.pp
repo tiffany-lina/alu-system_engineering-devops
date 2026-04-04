6e505e7d9a51144e9d92252d9f11a7d8d8c90e18
tiffany-lina <t.turate@alustudent.com>
2026-04-04 13:46:11 -0700

    Fix web stack: serve Holberton index via Nginx

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
+# 0-strace_is_your_friend.pp
+# Installs Apache and fixes WordPress configuration for Holberton page
+
 # Install Apache
 package { 'apache2':
   ensure => installed,
@@ -11,13 +14,13 @@ service { 'apache2':
 }
 
 # Fix WordPress config if missing
-exec { 'fix-wordpress':
+exec { 'fix-wordpress-config':
   command => 'mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php',
   onlyif  => 'test -f /var/www/html/wp-config-sample.php',
   path    => ['/bin', '/usr/bin'],
 }
 
-# Ensure web files are owned by www-data
+# Fix ownership for all web files
 file { '/var/www/html':
   ensure  => directory,
   recurse => true,
