c84e7cdad5ea23ad6b56ae30eef69b7ac9332ad0
tiffany-lina <t.turate@alustudent.com>
2026-04-04 13:37:07 -0700

    Fix Apache and WordPress for web stack debugging 3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Install Apache if missing
+# Install Apache
 package { 'apache2':
   ensure => installed,
 }
@@ -8,4 +8,19 @@ service { 'apache2':
   ensure => running,
   enable => true,
   require => Package['apache2'],
+}
+
+# Fix WordPress config if missing
+exec { 'fix-wordpress':
+  command => 'mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php',
+  onlyif  => 'test -f /var/www/html/wp-config-sample.php',
+  path    => ['/bin', '/usr/bin'],
+}
+
+# Ensure web files are owned by www-data
+file { '/var/www/html':
+  ensure  => directory,
+  recurse => true,
+  owner   => 'www-data',
+  group   => 'www-data',
 }
